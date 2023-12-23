ee7d5adda024e71262ce2fb25304b49767d07d42
Riccardo Prosdocimi <95254927+riccardoprosdocimi@users.noreply.github.com>
2023-12-23 17:39:46 +0100

    Refactoring done

---
+++ car/car.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+
+from serviceable import Serviceable
+
+
+class Car(ABC, Serviceable):
+    def __init__(self, engine, battery):
+        self.engine = engine
+        self.battery = battery
+
+    @abstractmethod
+    def needs_service(self):
+        pass
