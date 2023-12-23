2e32c757c70d3f9186ba1d077ee58ea2103b9027
Riccardo Prosdocimi <95254927+riccardoprosdocimi@users.noreply.github.com>
2023-12-23 22:21:27 +0100

    Update refactoring

--- battery/spindler_battery.py
+++ battery/spindler_battery.py
@@ -1,6 +1,5 @@
-from datetime import datetime
-
 from battery.battery import Battery
+from utils.utils import add_years_to_date
 
 
 class SpindlerBattery(Battery):
@@ -9,8 +8,8 @@ class SpindlerBattery(Battery):
         self.current_date = current_date
 
     def needs_service(self):
-        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 2)
-        if service_threshold_date < datetime.today().date():
+        service_threshold_date = add_years_to_date(self.last_service_date, 2)
+        if service_threshold_date < self.current_date:
             return True
         else:
             return False
